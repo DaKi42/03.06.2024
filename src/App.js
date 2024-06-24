2d1f5119e709f6ce5955fa175499ce2b2377ed87
Illia Bondar <kava.kav.23@gmail.com>
2024-06-24 20:23:54 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { NewArrivals, Promotions, InStockProducts, DeliveryAndPayment, Forums, AboutUs, NotFound  } from './components/Task1'
+import { MainPage } from './components/Task2';
+import { MainPage2 } from './components/Task3';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} >
+          {/* <Route path="/" element={<MainPage2 />} > */}
+          {/*компонент использует навигацию!*/}
+          <Route index element={<NewArrivals />} />
+          <Route path="new-arrivals" element={<NewArrivals />} />
+          <Route path="promotions" element={<Promotions />} />
+          <Route path="in-stock-products" element={<InStockProducts />} />
+          <Route path="delivery-and-payment" element={<DeliveryAndPayment />} />
+          <Route path="forums" element={<Forums />} />
+          <Route path="about-us" element={<AboutUs />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
+
   );
 }
 
